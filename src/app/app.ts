eeea9917480fc4c2da7e623f05b3e84886645108
Samantha <sam.j.maclaren@gmail.com>
2026-01-16 16:34:38 -0500

    feat / integrate footer component into app layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shell/header/header.component/header.component';
+import { FooterComponent } from './shell/footer/footer.component/footer.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     HeaderComponent,
+    FooterComponent,
     RouterOutlet
   ],
   templateUrl: './app.html',
