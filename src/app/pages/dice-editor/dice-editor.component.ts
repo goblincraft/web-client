3866efcd85603388544ca773a9892b6869879adf
Samantha <sam.j.maclaren@gmail.com>
2026-01-24 19:55:41 +1100

    feat / create die name input field

--- src/app/pages/dice-editor/dice-editor.component.ts
+++ src/app/pages/dice-editor/dice-editor.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-dice-editor',
   imports: [
+    MatInputModule,
+    MatSelectModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatFormFieldModule
   ],
   templateUrl: './dice-editor.component.html',
   styleUrl: './dice-editor.component.css',
