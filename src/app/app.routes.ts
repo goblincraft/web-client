903efae689c92a8401dfafbb95165cd1139408d4
Samantha <sam.j.maclaren@gmail.com>
2026-01-24 18:33:39 +1100

    feat / add route to dice editor from dice gallery

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,4 +3,5 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     { path: '', redirectTo: 'dice-dashboard', pathMatch: 'full' },
     { path: 'dice-dashboard', loadComponent: () => import('./pages/dice-dashboard/dice-dashboard.component').then(m => m.DiceDashboardComponent) },
+    { path: 'dice-editor', loadComponent: () => import('./pages/dice-editor/dice-editor.component').then(m => m.DiceEditorComponent) }
 ];
--- src/app/pages/dice-dashboard/features/dice-gallery/dice-gallery.component.ts
+++ src/app/pages/dice-dashboard/features/dice-gallery/dice-gallery.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from "@angular/router";
 
 @Component({
   selector: 'app-dice-gallery',
   imports: [
     MatCardModule,
     MatButtonModule,
-    MatIconModule
-  ],
+    MatIconModule,
+    RouterModule
+],
   templateUrl: './dice-gallery.component.html',
   styleUrl: './dice-gallery.component.css',
 })
