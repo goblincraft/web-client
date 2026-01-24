e7baff9b64cfba3b15baf3cb0d5f169ea4572677
Samantha <sam.j.maclaren@gmail.com>
2026-01-24 19:03:10 +1100

    style / update page action styling

--- src/app/pages/dice-editor/dice-editor.component.ts
+++ src/app/pages/dice-editor/dice-editor.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-dice-editor',
   imports: [
-    MatCardModule
+    MatCardModule,
+    MatButtonModule
   ],
   templateUrl: './dice-editor.component.html',
   styleUrl: './dice-editor.component.css',
