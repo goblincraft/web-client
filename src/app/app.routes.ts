05a59b17a93451a75c9295db65cd665f572b9eb2
Samantha <sam.j.maclaren@gmail.com>
2026-01-24 11:46:17 +1100

    refactor / change directory name to 'pages'

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'dice-dashboard', pathMatch: 'full' },
-    { path: 'dice-dashboard', loadComponent: () => import('./features/dice-dashboard/dice-dashboard.component').then(m => m.DiceDashboardComponent) },
+    { path: 'dice-dashboard', loadComponent: () => import('./pages/dice-dashboard/dice-dashboard.component').then(m => m.DiceDashboardComponent) },
 ];
