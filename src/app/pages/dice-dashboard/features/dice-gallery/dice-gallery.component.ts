4f98f3fbd37535389907d83f595d693087749649
Samantha <sam.j.maclaren@gmail.com>
2026-01-24 18:07:38 +1100

    feat / create dice gallery component

--- src/app/pages/dice-dashboard/features/dice-gallery/dice-gallery.component.ts
+++ src/app/pages/dice-dashboard/features/dice-gallery/dice-gallery.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-dice-gallery',
-  imports: [],
+  imports: [
+    MatCardModule
+  ],
   templateUrl: './dice-gallery.component.html',
   styleUrl: './dice-gallery.component.css',
 })
