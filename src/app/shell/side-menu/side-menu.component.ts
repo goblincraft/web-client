b87ee373dd49eb44c6a5a8bef950ea1cf061fc47
Samantha <sam.j.maclaren@gmail.com>
2026-01-24 11:58:25 +1100

    feat / create nav link for dice dashboard in side menu

--- src/app/shell/side-menu/side-menu.component.ts
+++ src/app/shell/side-menu/side-menu.component.ts
@@ -1,11 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-side-menu',
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatIconModule
+  ],
   templateUrl: './side-menu.component.html',
   styleUrl: './side-menu.component.css',
 })
 export class SideMenuComponent {
+
+  routes: { path: string; label: string, icon: string }[] = [
+    { path: 'dice-dashboard', label: 'Dice', icon: 'casino' }
+  ];
 
 }
