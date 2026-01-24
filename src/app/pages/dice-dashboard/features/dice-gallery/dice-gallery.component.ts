88bb95e613b158682ae7fbbe0e96c682e0e4b628
Samantha <sam.j.maclaren@gmail.com>
2026-01-24 18:23:11 +1100

    feat / update dice gallery styling

--- src/app/pages/dice-dashboard/features/dice-gallery/dice-gallery.component.ts
+++ src/app/pages/dice-dashboard/features/dice-gallery/dice-gallery.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-dice-gallery',
   imports: [
-    MatCardModule
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule
   ],
   templateUrl: './dice-gallery.component.html',
   styleUrl: './dice-gallery.component.css',
