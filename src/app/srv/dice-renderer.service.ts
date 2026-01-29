79b88f4946430eaa7639b758872884e76ff85a17
Samantha <sam.j.maclaren@gmail.com>
2026-01-29 13:07:50 +1000

    feat / update dice renderer create scene to include error handling

--- src/app/srv/dice-renderer.service.ts
+++ src/app/srv/dice-renderer.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import * as BABYLON from '@babylonjs/core';
+import { ToastService } from './toast.service';
 
 @Injectable({
   providedIn: 'root',
@@ -11,15 +12,22 @@ export class DiceRendererService {
   private camera: BABYLON.ArcRotateCamera | null = null;
   private light: BABYLON.HemisphericLight | null = null;
   private canvas: HTMLCanvasElement | null = null; 
+  private toastService = inject(ToastService);
   
-  createScene(canvas: HTMLCanvasElement): void {
-    this.canvas = canvas;
-    this.engine = new BABYLON.Engine(this.canvas, true);
-    this.scene = new BABYLON.Scene(this.engine);
-    this.camera = new BABYLON.ArcRotateCamera('camera', Math.PI / 2, Math.PI / 4, 10, new BABYLON.Vector3(0, 0, 0), this.scene);
-    this.camera.attachControl(this.canvas, true);
-    this.light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0, 1, 0), this.scene);
-    this.light.intensity = 0.7;
+  createScene(canvas: HTMLCanvasElement): boolean {
+    try {
+      this.canvas = canvas;
+      this.engine = new BABYLON.Engine(this.canvas, true);
+      this.scene = new BABYLON.Scene(this.engine);
+      this.camera = new BABYLON.ArcRotateCamera('camera', Math.PI / 2, Math.PI / 4, 10, new BABYLON.Vector3(0, 0, 0), this.scene);
+      this.camera.attachControl(this.canvas, true);
+      this.light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0, 1, 0), this.scene);
+      this.light.intensity = 0.7;
+      return true;
+    } catch (error) {
+      this.toastService.showErrorMessage('Failed to create dice renderer scene.');
+      return false;
+    }
   }
 
 }
