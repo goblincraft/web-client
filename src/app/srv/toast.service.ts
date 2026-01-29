a8f964fae2bf8a30185605b6a9a1e720bdafefa4
Samantha <sam.j.maclaren@gmail.com>
2026-01-29 13:04:55 +1000

    feat / implement error message toast

--- src/app/srv/toast.service.ts
+++ src/app/srv/toast.service.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ToastService {
+
+  toastr = inject(ToastrService);
   
+  showErrorMessage(message: string): void {
+    this.toastr.error(message);
+  }
+
 }
